694252ab3d99f17697cdfda11af069674e9a924d
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-07-02 08:26:14 +0200

    added new form svg

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 //* bookmark end*//
+//* toggle button
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".toggle-button");
+  const answerParagraph = document.querySelector(".answer");
+
+  toggleButton.addEventListener("click", () => {
+    answerParagraph.classList.toggle("show-answer");
+  });
+});
+
+//* end toggle button *//
